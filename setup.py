cdcb4facde6f712757d7c6d8311c4967e75e8397
waffelz <wyattmullis52511@gmail.com>
2024-11-13 20:08:25 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ComplexPy",
+    version="v0.0.1",
+    packages=find_packages(),
+    description="A Python library for handling calculations of complex number.",
+    author="waffelz",
+    author_email="**Don't contact me via email** -- use discord instead, my user is @waffelz0212",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6"
+)
